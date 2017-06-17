b09765a76c1c1b31495fde7e5253900931983634
Ferran Garriga <zegnus@gmail.com>
2017-06-17 11:22:53 +0100

    Initialise lidar and radar measurements

--- p6-extended-kalman-filters/src/FusionEKF.cpp
+++ p6-extended-kalman-filters/src/FusionEKF.cpp
@@ -22,18 +22,18 @@ FusionEKF::FusionEKF() {
   H_laser_ = MatrixXd(2, 4);
   Hj_ = MatrixXd(3, 4);
 
+  //measurement matrix that will remove the velocity from the measurement from Lidar
+  H_laser_ << 1, 0, 0, 0,
+              0, 1, 0, 0;
+
   //measurement covariance matrix - laser
-  R_laser_ << 0.0225, 0, 0, 0.0225;
+  R_laser_ << 0.0225, 0,
+              0, 0.0225;
 
   //measurement covariance matrix - radar
-  R_radar_ << 0.09, 0, 0, 0, 0.0009, 0, 0, 0, 0.09;
-
-  /**
-   TODO:
-   * Finish initializing the FusionEKF.
-   * Set the process and measurement noises
-   */
-
+  R_radar_ << 0.09, 0, 0,
+              0, 0.0009, 0,
+              0, 0, 0.09;
 }
 
 /**
@@ -48,28 +48,53 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
    *  Initialization
    ****************************************************************************/
   if (!is_initialized_) {
-    /**
-     TODO:
-     * Initialize the state ekf_.x_ with the first measurement.
-     * Create the covariance matrix.
-     * Remember: you'll need to convert radar from polar to cartesian coordinates.
-     */
-    // first measurement
     cout << "EKF: " << endl;
+
+    // Initialise position and velocity
+
     ekf_.x_ = VectorXd(4);
-    ekf_.x_ << 1, 1, 1, 1;
+
+    float position_x = 1;
+    float position_y = 1;
+    float velocity_x = 1;
+    float velocity_y = 1;
 
     if (measurement_pack.sensor_type_ == MeasurementPackage::RADAR) {
       /**
        Convert radar from polar to cartesian coordinates and initialize state.
        */
+      float horizontal_projection = cos(measurement_pack.raw_measurements_[1]);
+      float vertical_projection = sin(measurement_pack.raw_measurements_[1]);
+
+      position_x = measurement_pack.raw_measurements_[0] * horizontal_projection;
+      position_y = measurement_pack.raw_measurements_[0] * vertical_projection;
+      velocity_x = measurement_pack.raw_measurements_[2] * horizontal_projection;
+      velocity_y = measurement_pack.raw_measurements_[2] * vertical_projection;
     } else if (measurement_pack.sensor_type_ == MeasurementPackage::LASER) {
-      /**
-       Initialize state.
-       */
+      position_x = measurement_pack.raw_measurements_[0];
+      position_y = measurement_pack.raw_measurements_[1];
     }
 
-    // done initializing, no need to predict or update
+    if (position_x == 0) position_x = 0.0001;
+    if (position_y == 0) position_y = 0.0001;
+
+    ekf_.x_ << position_x, position_y, velocity_x, velocity_y;
+
+    cout << "EKF init: " << ekf_.x_ << endl;
+
+    // Initialise transition state with hight uncertainty covariance matrix P
+
+    ekf_.P_ = MatrixXd(4, 4);
+    ekf_.P_ << 1000, 0, 0, 0,
+                0, 1000, 0, 0,
+                0, 0, 1000, 0,
+                0, 0, 0, 1000;
+
+
+    // Initialise timestamp
+
+    previous_timestamp_ = measurement_pack.timestamp_;
+
     is_initialized_ = true;
     return;
   }
